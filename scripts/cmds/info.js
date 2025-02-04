2c2a5222d59074eb9c9414fa516ba7ba65688414
Sorwar hosen Sohan <75998562+sohanvai@users.noreply.github.com>
2025-02-04 22:51:24 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -14,53 +14,56 @@ module.exports = {
 		guide: { en: "" },
 		envConfig: {}
 	},
+
 	onStart: async function ({ message }) {
-		const authorName = " 𝑰𝒕achi 𝑼𝒄𝒉𝒊𝒉𝒂❟❛❟ ";
-		const ownAge = "『 16+ 』";
-		const messenger = "nai Jah Vhag🙂";
-		const authorFB = "iccha nai amar😫";
-		const authorNumber = "01888240943";
-		const Status = "⩸__🆂🅸🅽🅶🅻🅴__⩸";
-		const urls = [
-"https://i.imgur.com/e74rsW1.mp4",
-"https://i.imgur.com/e74rsW1.mp4",
-"https://i.imgur.com/e74rsW1.mp4",
-"https://i.imgur.com/e74rsW1.mp4"
-];
-		const link = urls[Math.floor(Math.random() * urls.length)];
+		const ownerInfo = {
+			name: " 𝑶𝒃𝒊𝒕𝒐 𝑼𝒄𝒉𝒊𝒉𝒂❟❛❟ ",  // Updated Name
+			age: "『 16+ 』",
+			status: "⩸__🆂🅸🅽🅶🅻🅴__⩸",
+			whatsapp: "01888240943",
+			facebook: "iccha nai amar😫",
+			messenger: "nai Jah Vhag🙂"
+		};
+
+		const botInfo = {
+			name: global.GoatBot.config.nickNameBot,
+			prefix: global.GoatBot.config.prefix
+		};
+
+		const videoLink = "https://i.imgur.com/6jNIPSg.mp4";
+
 		const now = moment().tz('Asia/Jakarta');
 		const date = now.format('MMMM Do YYYY');
 		const time = now.format('h:mm:ss A');
+
 		const uptime = process.uptime();
-		const seconds = Math.floor(uptime % 60);
-		const minutes = Math.floor((uptime / 60) % 60);
-		const hours = Math.floor((uptime / (60 * 60)) % 24);
-		const days = Math.floor(uptime / (60 * 60 * 24));
-		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+		const uptimeString = `${Math.floor(uptime / (60 * 60 * 24))}d ${Math.floor((uptime / (60 * 60)) % 24)}h ${Math.floor((uptime / 60) % 60)}m ${Math.floor(uptime % 60)}s`;
 
 		message.reply({
-			body: `💫《 ⩸__𝐁𝐨𝐭 𝐀𝐧𝐝 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧__⩸ 》💫
-\🤖彡𝐵𝑜𝑡 𝑁𝑎𝑚𝑒 : ⩸__${global.GoatBot.config.nickNameBot}__⩸
-\👾彡𝐵𝑜𝑡 𝑆𝑦𝑠𝑡𝑒𝑚 𝑃𝑟𝑒𝑓𝑖𝑥 : ${global.GoatBot.config.prefix}
-\💙彡𝑂𝑤𝑛𝑒𝑟 𝑁𝑎𝑚𝑒 : ${authorName}
-\📝彡𝐴𝑔𝑒  : ${ownAge}
-\💕彡𝑅𝑒𝑙𝑎𝑡𝑖𝑜𝑛𝑆ℎ𝑖𝑝: ${Status}
-\🌐彡𝑊𝑝 : ${authorNumber}
-\🌍彡𝐹𝑎𝑐𝑒𝑏𝑜𝑜𝑘 𝐿𝑖𝑛𝑘 : ${authorFB}
-\🗓彡𝐷𝑎𝑡𝑒 : ${date}
-\⏰彡𝑁𝑜𝑤 𝑇𝑖𝑚𝑒 : ${time}
-\🔰彡𝐴𝑛𝑦 𝐻𝑒𝑙𝑝 𝐶𝑜𝑛𝑡𝑎𝑐𝑡 :⩸__${messenger}__⩸
-\📛彡𝐵𝑜𝑡 𝐼𝑠 𝑅𝑢𝑛𝑛𝑖𝑛𝑔 𝐹𝑜𝑟 : ${uptimeString}
-    𝑻𝒈: Nai re vhai🙂
-    𝑰𝒏𝒔𝒕𝒂: deskofsohan🙂
-    𝑪𝒂𝒑𝑪𝒖𝒕: Impossible 😫
-    𝑻𝒊𝒌𝑻𝒐𝒌: Ami Ki Prothi bondi naki🙂?
-    𝒀𝒐𝒖𝑻𝒖𝒃𝒆: 🤨
+			body: `💫《 ⩸__𝐁𝐨𝐭 & 𝐎𝐰𝐧𝐞𝐫 𝐈𝐧𝐟𝐨__⩸ 》💫
+\🤖 𝐵𝑜𝑡 𝑁𝑎𝑚𝑒 : ⩸__${botInfo.name}__⩸
+\👾 𝑃𝑟𝑒𝑓𝑖𝑥 : ${botInfo.prefix}
+\💙 𝑂𝑤𝑛𝑒𝑟 : ${ownerInfo.name}
+\📝 𝐴𝑔𝑒 : ${ownerInfo.age}
+\💕 𝑅𝑒𝑙𝑎𝑡𝑖𝑜𝑛𝑆ℎ𝑖𝑝 : ${ownerInfo.status}
+\🌐 𝑊ℎ𝑎𝑡𝑠𝐴𝑝𝑝 : ${ownerInfo.whatsapp}
+\🌍 𝐹𝑎𝑐𝑒𝑏𝑜𝑜𝑘 : ${ownerInfo.facebook}
+\🗓 𝐷𝑎𝑡𝑒 : ${date}
+\⏰ 𝑇𝑖𝑚𝑒 : ${time}
+\📛 𝑈𝑝𝑡𝑖𝑚𝑒 : ${uptimeString}
+
+🔗 𝑂𝑡ℎ𝑒𝑟 𝑆𝑜𝑐𝑖𝑎𝑙𝑠:
+📌 𝑻𝒈: Nai re vhai🙂  
+📌 𝑰𝒏𝒔𝒕𝒂: deskofsohan🙂  
+📌 𝑪𝒂𝒑𝑪𝒖𝒕: Impossible 😫  
+📌 𝑻𝒊𝒌𝑻𝒐𝒌: Ami Ki Prothi bondi naki🙂?  
+📌 𝒀𝒐𝒖𝑻𝒖𝒃𝒆: 🤨  
 \===============`,
-			attachment: await global.utils.getStreamFromURL(link)
+			attachment: await global.utils.getStreamFromURL(videoLink)
 		});
 	},
-	onChat: async function ({ event, message, getLang }) {
+
+	onChat: async function ({ event, message }) {
 		if (event.body && event.body.toLowerCase() === "info") {
 			this.onStart({ message });
 		}
